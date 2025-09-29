8161b37eafad360b859ec1a81117643fb15917be
Dominique Yemdom ngueussie <88015764+dominiquengueussie@users.noreply.github.com>
2025-09-29 08:53:42 +0200

    Update PromoProducts.vue

--- components/Home/PromoProducts.vue
+++ components/Home/PromoProducts.vue
@@ -62,8 +62,13 @@ const showProductDetails = (product) => {
         <h2 class="text-3xl font-bold text-gray-800">Produits Populaire </h2>
         <p class="text-gray-600">Profitez de nos offres spéciales</p>
       </div>
-      <NuxtLink to="/store" class="text-slate-900 hover:text-gold font-medium flex items-center">
-        Voir tout <ChevronRight /></NuxtLink>
+    <NuxtLink
+  to="/store"
+  class="text-slate-900 hover:text-gold font-medium flex items-center"
+>
+  Voir tout <ChevronRight />
+</NuxtLink>
+
     </div>
     
     <div v-if="isLoading" class="flex justify-center items-center min-h-[200px]">
